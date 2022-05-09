55813f1c0ea1e6503e57caa5247eb90d8e239e43
ManuMxrales <manumxrales@gmail.com>
2022-05-09 17:36:31 -0500

    modelos 1.0

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        isAlpha: true,
+    }
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subRegion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    poblacion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,18 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const countriesRouter = require('./countries.router.js');
+const { activitiesRouter } = require('./activities.router.js');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use('/countries', countriesRouter);
+
+
 
 
 module.exports = router;
