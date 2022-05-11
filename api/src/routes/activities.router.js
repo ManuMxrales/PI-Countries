55800775d2694fa088b0dcd2c6cfd82b7d27c8d8
ManuMxrales <manumxrales@gmail.com>
2022-05-11 18:35:00 -0500

    work in progress

--- api/src/routes/activities.router.js
+++ api/src/routes/activities.router.js
@@ -5,7 +5,8 @@ const ActivitieService = require('../services/activities.services.js');
 const service = new ActivitieService();
 
 router.post('/', async (req, res) => {
-    const act = await service.createActivitie();
+    const { name, difficulty, duration, season, pais} = req.body;
+    const act = await service.createActivitie(name, difficulty, duration, season, pais);
     res.json(act);
 })
 
