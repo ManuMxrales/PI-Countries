fd725a423ab98822f1ce3eb2cfcf45b1f0fffd83
ManuMxrales <manumxrales@gmail.com>
2022-05-11 18:35:28 -0500

    work in progress

--- api/src/services/activities.services.js
+++ api/src/services/activities.services.js
@@ -1,6 +1,24 @@
+const { Country, Activities } = require("../db");
+const { Op } = require('sequelize');
 class ActivitieService {
-    createActivitie(){
-        return 'creamos actividad';
+
+    async createActivitie(name, difficulty, duration, season, pais){
+        const actividad = await Activities.create({
+            name: name,
+            difficulty: difficulty,
+            duration: duration,
+            season: season
+        });
+        pais.forEach(async (p) => {
+            let act = await Country.findOne({
+                where: {
+                    id: p
+                }
+            })
+            await actividad.addCountry(act);
+        });
+        
+        return "Actividad Agregada Exitosamente";
     }
 }
 module.exports = ActivitieService;
