6ad3e56ab840aab6390c25a7808965e18acbcf4d
ManuMxrales <manumxrales@gmail.com>
2022-05-11 18:08:42 -0500

    Services de Countries Terminados v1

--- api/src/services/country.services.js
+++ api/src/services/country.services.js
@@ -1,28 +1,36 @@
 const axios = require("axios");
-const e = require("express");
-const { Sequelize } = require("sequelize");
-const { Country } = require("../db");
+const { Country, Activities } = require("../db");
+const { Op } = require('sequelize');
 
 class CountryService {
   constructor() {
     this.getApiCountries();
   }
   async getApiCountries() {
-    const con = await axios.get("https://restcountries.com/v3/all");
-    const info = await con.data.map((c) => {
-      return {
-        id: c.cca3,
-        name: c.name.official,
-        image: c.flags[0],
-        continents: Array.isArray(c.continents)? c.continents[0] : c.continents,
-        capital: typeof c.capital === 'object'? c.capital[0] : "Capital no encontrada",
-        subregion: c.subregion,
-        area: c.area,
-        population: c.population,
-      };
-    });
-    Country.bulkCreate(info);
-    return info;
+    try {
+      const con = await axios.get("https://restcountries.com/v3/all");
+      const info = await con.data.map((c) => {
+        return {
+          id: c.cca3,
+          name: c.name.common.toLowerCase(),
+          image: c.flags[0],
+          continents: Array.isArray(c.continents)
+            ? c.continents[0]
+            : c.continents,
+          capital:
+            typeof c.capital === "object"
+              ? c.capital[0]
+              : "Capital no encontrada",
+          subregion: c.subregion,
+          area: c.area,
+          population: c.population,
+        };
+      });
+      Country.bulkCreate(info);
+      return info;
+    } catch (error) {
+      throw Error(error);
+    }
   }
   async getCountries() {
     try {
@@ -32,11 +40,35 @@ class CountryService {
       throw Error(error);
     }
   }
-  findOne(id) {
-    return "Aca retornamos solo un pais" + id;
+
+  async find(name) {
+    try {
+    const instancias = await Country.findAll({
+        where: {
+          name: {[Op.substring]:name},
+        },
+        include: {
+          model: Activities,
+        }
+      });
+      return instancias.length? instancias : 'No existe un pais con ese nombre';
+    } catch (error) {
+      throw Error(error.message);
+    }
   }
-  find(name) {
-    return "Obtener paises pasados con el query" + name;
+
+async findOne(id) {
+    try {
+      let realId = id.substring(1);
+      const instancias = await Country.findByPk(realId,{
+        include: {
+          model: Activities,
+        }
+      });
+     return instancias? instancias : 'No existe un pais con ese ID';
+    } catch (error) {
+      throw Error(error.message);
+    }
   }
 }
 module.exports = CountryService;
