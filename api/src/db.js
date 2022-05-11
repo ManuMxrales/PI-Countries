9e30329ad98c1fe9ac4c38d726c6289e037ddea3
ManuMxrales <manumxrales@gmail.com>
2022-05-11 18:05:18 -0500

    Rutas get terminadas

--- api/src/db.js
+++ api/src/db.js
@@ -37,7 +37,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon, Country, Activities, Countryactivities } = sequelize.models;
+const { Country, Activities, Countryactivities } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
