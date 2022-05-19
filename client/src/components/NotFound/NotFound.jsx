97edf306c9795444290cb9a4f2bad47455498a54
ManuMxrales <manumxrales@gmail.com>
2022-05-18 20:37:41 -0500

    404 component

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import s from './NotFound.module.css';
+// import { Link } from 'react-router-dom';
 export const NotFound = () => {
   return (
-    <h1>NotFound 404</h1>
+    <div className={s.NotFoundContainer}>
+      <h1>NotFound 404</h1>
+      <p>Ese pais no existe (aun?)</p>
+      {/* <Link to={"/ruta-principal"}>
+        <button className={s.btn}>Regresar</button>
+      </Link> */}
+    </div>
   )
 }
