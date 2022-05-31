69126aaefd214543f27348c217f18a2425e28999
ManuMxrales <manumxrales@gmail.com>
2022-05-30 19:16:41 -0500

    Cambio en la Pagina Principal

--- client/src/components/Inicial.jsx
+++ client/src/components/Inicial.jsx
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 export const Inicial = () => {
   return (
     <div className={s.pagina_inicial}>
-        <h1 className={s.pagina_inicial_title}>Henry Countries</h1>
+        <h1 className={s.pagina_inicial_title}>Countries</h1>
+        <p>Hecho con  &#128155;</p>
         <Link to={"/ruta-principal"}>
         <button className={s.pagina_inicial_btn}>Comenzar</button>
         </Link>
